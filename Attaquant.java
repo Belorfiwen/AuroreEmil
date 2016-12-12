a2b9269d349589b0aaaf29244dff162f1b7ce67d
koukie <aurore.garnier2424@orange.fr>
2016-12-12 16:33:21 +0100

    aurore1

---
+++ Attaquant.java
@@ -0,0 +1,12 @@
+public abstract class Attaquant extends Personnage
+{
+	int nbAttaquant = 3;
+	
+	// ajouter les classes move (), shoot ()
+	
+	public String toString ()
+	{
+		return "?";
+		//comparer si e1 "^" si e2 "v"
+	}
+}
