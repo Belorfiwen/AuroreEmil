b25e40dd6e9951407ff79ba1f5d7d4aea755057d
Emilien Cholley <roller-emilien-kaze@MacBook-Pro-de-Emilien.local>
2016-12-12 18:14:38 +0100

    lundi 18H

--- But.java
+++ But.java
@@ -4,6 +4,7 @@ public class But
 
 	But 
 	{
-
+		this.positionA = ((terrain.ligne - terrain.largeurBut)/2) -1;
+		this.positionB = ((terrain.ligne - terrain.largeurBut)/2) + terrain.largeurBut -1;
 	}
 }
--- Terrain.java
+++ Terrain.java
@@ -1,11 +1,30 @@
 public class Terrain
 {
 	private int ligne, colonne, largeurBut;
-	private ArrayList <Jeu> grille;
+	private Jeu [][] grilleLigne;
 
-	Terrain 
+	Terrain ()
 	{
-
+		this.ligne = 20;
+		this.colonne = 40;
+		this.largeurBut = 14;
+		this.grilleLigne = new <ArrayList <Jeu>> ();
+		for (int i=0; i<Ligne;i++) 
+		{
+			this.grilleColonne = new <ArrayList <Jeu>> ();
+		}
+		
+	}
+	Terrain (int ligne, int colonne, int largeurBut)
+	{
+		this.ligne = ligne;
+		this.colonne = colone;
+		this.largeurBut = largeurBut;
+		this.grilleLigne = new <ArrayList <Jeu>> ();
+		for (int i=0; i<Ligne;i++) 
+		{
+			this.grilleColonne = new <ArrayList <Jeu>> ();
+		}
 	}
 
 	void evolve ()
